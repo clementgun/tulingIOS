ca18a0c8eba6b0e9c021a5b25d1446b882080508
colin <hitwhylz@163.com>
2014-08-09 17:57:12 +0800

    Initial Commit

---
+++ tulingIOSTests/tulingIOSTests.m
@@ -0,0 +1,34 @@
+//
+//  tulingIOSTests.m
+//  tulingIOSTests
+//
+//  Created by Colin on 14-8-9.
+//  Copyright (c) 2014年 icephone. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tulingIOSTests : XCTestCase
+
+@end
+
+@implementation tulingIOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
